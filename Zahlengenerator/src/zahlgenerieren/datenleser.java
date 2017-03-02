1f01d21b7e75b9eb23af9f6bfcdd232ef17dd4a0
antiidlecoder <Joel@Joels-MacBook-Pro.local>
2017-03-02 17:01:45 +0100

    updated mathematiker & datenleser

--- Zahlengenerator/src/zahlgenerieren/datenleser.java
+++ Zahlengenerator/src/zahlgenerieren/datenleser.java
@@ -1,10 +1,43 @@
-package zahlgenerieren;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class datenleser {
-	
-	private static final String FILENAME = "/Users/streber/Documents/workspace/Modul226/Zahlengenerator/zahlendestages.txt";
-
-	public static String getFilename() {
-		return FILENAME;
-	} 
+public class DatenLeser {
+    
+    
+    public int[]  liesZahlen(){
+        
+        int[] array = new int[100];
+        
+        
+        File dataFile = new File("zahlendestages2.txt");
+        
+        try{
+            BufferedReader reader = new BufferedReader( new FileReader(dataFile));
+            
+            int position = 0;
+            String line;
+            
+            
+            while ((line = reader.readLine()) != null) {
+                array[position] = Integer.parseInt(line);
+                position++;
+            }
+            reader.close();
+            
+            for (int i = 0; i < array.length; i++){
+                System.out.println(array[i]);
+            }
+            
+        }
+        catch (IOException e){
+            System.out.println("Datei nicht gefunden");
+        }
+        
+        return array;
+        
+    }
+    
+    
 }
--- Zahlengenerator/src/zahlgenerieren/mathematiker.java
+++ Zahlengenerator/src/zahlgenerieren/mathematiker.java
@@ -1,5 +1,48 @@
-package zahlgenerieren;
 
-public class mathematiker {
-
+public class Mathematiker {
+    
+    private int[] zahlenSammlung;
+    
+    public Mathematiker(int[] zahlenSammlung){
+        this.zahlenSammlung = zahlenSammlung;
+    }
+    
+    public void getMax(){
+        int max = 0;
+        for (int i = 0; i < zahlenSammlung.length; i++){
+            if (zahlenSammlung[i] > max){
+                max = zahlenSammlung[i];
+            }
+        }
+        System.out.println("Die grösste Zahl der Liste lautet: " + max);
+    }
+    
+    public void getMin(){
+        int min = 999;
+        for (int i = 0; i < zahlenSammlung.length; i++){
+            if (zahlenSammlung[i] < min){
+                min = zahlenSammlung[i];
+            }
+        }
+        System.out.println("Die kleinste Zahl der Liste lautet: " + min);
+    }
+    
+    public void getSum(){
+        int sum = 0;
+        for (int i = 0; i < zahlenSammlung.length; i++){
+            sum += zahlenSammlung[i];
+        }
+        System.out.println("Die Summe aller Zahlen der Liste lautet: " + sum);
+    }
+    
+    public void getAverage(){
+        int sum = 0;
+        double average = 0;
+        for (int i = 0; i < zahlenSammlung.length; i++){
+            sum += zahlenSammlung[i];
+        }
+        average = sum / zahlenSammlung.length;
+        System.out.println("Der Durchschnitt aller Zahlen der Liste lautet: " + average);
+    }
+    
 }
